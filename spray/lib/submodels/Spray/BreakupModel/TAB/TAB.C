4fc67fa26c82396eb7bd1728864eebc1204e54bf
Niklas Nordin <niklas.nordin@nequam.se>
2011-04-11 20:07:15 +0200

    added TAB breakup model framework

---
+++ spray/lib/submodels/Spray/BreakupModel/TAB/TAB.C
@@ -0,0 +1,67 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | Copyright (C) 2009-2010 OpenCFD Ltd.
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+\*---------------------------------------------------------------------------*/
+
+#include "TAB.H"
+
+// * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
+
+template <class CloudType>
+Foam::TAB<CloudType>::TAB
+(
+    const dictionary&,
+    CloudType& owner
+)
+:
+    BreakupModel<CloudType>(owner)
+{}
+
+
+// * * * * * * * * * * * * * * * * Destructor  * * * * * * * * * * * * * * * //
+
+template <class CloudType>
+Foam::TAB<CloudType>::~TAB()
+{}
+
+
+// * * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * //
+
+template<class CloudType>
+void Foam::TAB<CloudType>::breakup
+(
+    const scalar& dt,
+    scalar& d,
+    const scalar& rho,
+    const scalar& mu,
+    const scalar& sigma,
+    const scalar& rhoc,
+    const scalar& muc,
+    const scalar& Urel
+) const
+{
+  // Do nothing
+}
+
+
+// ************************************************************************* //
