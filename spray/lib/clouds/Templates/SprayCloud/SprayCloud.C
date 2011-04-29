a83f22a00a7ed4f1ab5c4b66fe8f01b477234d87
Niklas Nordin <niklas.nordin@nequam.se>
2011-04-29 13:03:18 +0200

    added liquid penetration info

--- spray/lib/clouds/Templates/SprayCloud/SprayCloud.C
+++ spray/lib/clouds/Templates/SprayCloud/SprayCloud.C
@@ -293,7 +293,6 @@ void Foam::SprayCloud<ParcelType>::checkParcelProperties
     parcel.yDot() = breakup().yDot0();
 }
 
-
 template<class ParcelType>
 Foam::scalar Foam::SprayCloud<ParcelType>::D(const label i, const label j) const
 {
@@ -310,6 +309,96 @@ Foam::scalar Foam::SprayCloud<ParcelType>::D(const label i, const label j) const
     reduce(sj, sumOp<scalar>());
     sj = max(sj, VSMALL);
     return si/sj;
+}
+
+
+template<class ParcelType>
+Foam::scalar Foam::SprayCloud<ParcelType>::liquidPenetration(const scalar& prc) const
+{
+
+    scalar distance = 0.0;
+    scalar mTot = 0.0;
+
+    label Np = this->size();
+    // arrays containing the parcels mass and
+    // distance from injector in ascending order
+    scalarField mass(Np);
+    scalarField dist(Np);
+    label n = 0;
+
+    // first arrange the parcels in ascending order
+    // the first parcel is closest to its injection position
+    // and the last one is most far away.
+    forAllConstIter(typename Cloud<ParcelType>, *this, iter)
+    {
+        const ParcelType& p = iter();
+	scalar mi = p.nParticle()*p.mass();
+	scalar di = mag(p.position() - p.position0());
+	mTot += mi;
+
+	// insert at the last place
+	mass[n] = mi;
+	dist[n] = di;
+
+	label i = 0;
+	bool found = false;
+
+	// insert the parcel in the correct place
+	// and move the others 
+	while ( ( i < n ) && ( !found ) ) 
+	{
+	    if (di < dist[i])
+	    {
+	        found = true;
+		for(label j=n; j>i; j--)
+		{
+		    mass[j] = mass[j-1];
+		    dist[j] = dist[j-1];
+		}
+		mass[i] = mi;
+		dist[i] = di;
+	    }
+	    i++;
+	}
+	n++;
+    }
+
+    reduce(mTot, sumOp<scalar>());
+
+    scalar mLimit = prc*mTot;
+    scalar mOff = (1.0 - prc)*mTot;
+
+    if (Np > 1)
+    {
+
+        // 'prc' is large enough that the parcel most far
+        // away will be used, no need to loop...
+        if (mLimit > mTot - mass[Np-1])
+        {
+            distance = dist[Np-1];
+        }
+        else
+        {
+            scalar mOffSum = 0.0;
+            label i = Np;
+
+            while ((mOffSum < mOff) && (i>0))
+            {
+                i--;
+                mOffSum += mass[i];
+            }
+            distance = dist[i] + (dist[i+1]-dist[i])*(mOff - mOffSum)/mass[i+1] ;
+        }
+
+    }
+    else
+    {
+        distance = dist[0];
+    }
+
+    reduce(distance, maxOp<scalar>());
+
+    return distance;
 }
 
 template<class ParcelType>
@@ -341,7 +430,10 @@ void Foam::SprayCloud<ParcelType>::info() const
 {
     ReactingCloud<ParcelType>::info();
     scalar d32 = 1.0e+6*D(3,2);
+    scalar pen = liquidPenetration(0.95);
+
     Info << "    D32 (mu)                        = " << d32 << endl;
+    Info << "    liquid penetration 95% mass (m) = " << pen << endl;
 }
 
 
