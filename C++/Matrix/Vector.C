642bb4ddc2b0a0f8ed042f5232333911d2f17a07
Niklas Nordin <niklas.nordin@nequam.se>
2012-03-14 21:21:35 +0100

    added some basic operators

--- C++/Matrix/Vector.C
+++ C++/Matrix/Vector.C
@@ -7,5 +7,35 @@ Vector::Vector()
     z_(0)
 {}
 
+Vector::Vector
+(
+    const scalar& x,
+    const scalar& y,
+    const scalar& z
+)
+:
+    x_(x), y_(y), z_(z)
+{}
+
 Vector::~Vector()
 {}
+
+double Vector::operator&(const Vector& v) const
+{
+    return x_*v.x() +y_*v.y() + z_*v.z();
+}
+
+Vector Vector::operator+(const Vector& v) const
+{
+    return Vector(x_+v.x(), y_+v.y(), z_+v.z());
+}
+
+Vector Vector::operator-(const Vector& v) const
+{
+  return Vector(x_-v.x(), y_-v.y(), z_-v.z());
+}
+
+Vector Vector::operator*(const scalar& a) const
+{
+    return Vector(a*x_, a*y_, a*z_);
+}
