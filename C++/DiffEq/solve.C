ae16efc2366f8b3a9230064adecadf44c044f66e
Niklas Nordin <niklas.nordin@nequam.se>
2012-06-27 20:47:13 +0200

    fixed Makefile for static library and added some functionality to the nBody-class

--- C++/DiffEq/solve.C
+++ C++/DiffEq/solve.C
@@ -1,11 +1,66 @@
+/*
+  This program is intended for two things
+  Solve the differential equation using 4-5 th order Runge-Kutta method of:
+  1. The spring-mass-damper equation with a random force
+  2. Solve the n-body problem.
+*/
+
 #include <iostream>
+#include <vector>
+#include "Vector.H"
+
+typedef double scalar;
+typedef int label;
+
+class nBody
+{
+private:
+  label nBodies_;
+  vector<Vector> positions_;
+  vector<Vector> velocities_;
+  vector<scalar> mass_;
+
+public:
+  nBody(const label nBodies)
+  :
+    nBodies_(nBodies),
+    positions_(nBodies),
+    velocities_(nBodies),
+    mass_(nBodies)
+  {}
+
+  ~nBody()
+  {}
+
+  const vector<Vector>& positions() const
+  {
+    return positions_;
+  }
+
+  vector<Vector>& positions()
+  {
+    return positions_;
+  }
+
+};
+
+class spring
+{
+};
 
 using namespace std;
 
 int main()
 {
+  scalar a = 1.0;
+  label nBodies = 3;
+  nBody system(nBodies);
 
-  cout << "hello world" << endl;
+  cout << "hello world, a = " << a << endl;
+  
+  label size = system.positions().size();
+
+  cout << "size = " << size << endl;
 
   return 0;
 }
