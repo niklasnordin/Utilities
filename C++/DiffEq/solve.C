33031374e7c2f72cac5f7bc47ff26bf1ec799f1a
Niklas Nordin <niklas.nordin@nequam.se>
2012-06-29 20:38:55 +0200

    step 1 finished

--- C++/DiffEq/solve.C
+++ C++/DiffEq/solve.C
@@ -17,144 +17,164 @@ typedef int label;
 
 // gravitational constant
 //#define G 6.672e-11
-#define G 6.672
+#define G 6.672e-10
 
 class nBody
 {
 private:
-  label nBodies_;
-  vector<Vector> positions_;
-  vector<Vector> velocities_;
-  vector<scalar> mass_;
+    label nBodies_;
+    vector<Vector> positions_;
+    vector<Vector> velocities_;
+    vector<scalar> mass_;
 
 public:
 
-  // constructor
-  nBody(const label nBodies)
-  :
-    nBodies_(nBodies),
-    positions_(nBodies),
-    velocities_(nBodies),
-    mass_(nBodies)
-  {}
+    // constructor
+    nBody(const label nBodies)
+    :
+      nBodies_(nBodies),
+      positions_(nBodies),
+      velocities_(nBodies),
+      mass_(nBodies)
+    {}
 
-  // destructor
-  ~nBody()
-  {}
+    // destructor
+    ~nBody()
+    {}
 
-  // access functions
-  const vector<Vector>& positions() const
-  {
-    return positions_;
-  }
-
-  vector<Vector>& positions()
-  {
-    return positions_;
-  }
-
-  const vector<Vector>& velocities() const
-  {
-    return velocities_;
-  }
-
-  vector<Vector>& velocities()
-  {
-    return velocities_;
-  }
-
-  const vector<scalar>& mass() const
-  {
-    return mass_;
-  }
-  
-  vector<scalar>& mass()
-  {
-    return mass_;
-  }
-
-  vector<Vector> forces
-  (
-    const vector<Vector>& positions
-  )
-  {
-    Vector zero = Vector(0,0,0);
-
-    vector<Vector> forces(positions.size());
-    for(label i=0; i<positions.size(); i++)
+    // access functions
+    const vector<Vector>& positions() const
     {
-	forces[i] = zero;
+        return positions_;
     }
 
-    for(label i=0; i<positions.size()-1; i++)
+    vector<Vector>& positions()
     {
-	for(label j=i+1; j<positions.size(); j++)
-	{
-	    Vector rad = positions[i] - positions[j];
+        return positions_;
+    }
+
+    const vector<Vector>& velocities() const
+    {
+        return velocities_;
+    }
+
+    vector<Vector>& velocities()
+    {
+        return velocities_;
+    }
+
+    const vector<scalar>& mass() const
+    {
+        return mass_;
+    }
+  
+    vector<scalar>& mass()
+    {
+        return mass_;
+    }
+
+    vector<Vector> forces
+    (
+        const vector<Vector>& pos
+    )
+    {
+        Vector zero = Vector(0,0,0);
+
+        vector<Vector> forces(pos.size());
+        for(label i=0; i<pos.size(); i++)
+        {
+	    forces[i] = zero;
+        }
+
+	scalar rad2Min = 1.0e-1;
+
+        for(label i=0; i<pos.size()-1; i++)
+        {
+	    for(label j=i+1; j<pos.size(); j++)
+	    {
+	        Vector rad = pos[i] - pos[j];
 	    
-	    scalar rad2 = rad & rad;
-	    scalar mag = sqrt(rad2);
-	    Vector n = rad/(mag + SMALL);
+                scalar rad2 = rad & rad;
+                scalar mag = sqrt(rad2);
+                Vector n = rad/(mag + SMALL);
+		if (rad2 < rad2Min) rad2 = rad2Min;
+                Vector fij = (G * mass_[i] * mass_[j] / rad2 ) * n;
+                forces[i] = forces[i] - fij;
+                forces[j] = forces[j] + fij;
+            }
+        } 
 
-	    Vector fij = G * mass_[i] * mass_[j] * n / ( rad2 + SMALL );
-	    forces[i] = forces[i] + fij;
-	    forces[j] = forces[j] - fij;
+        return forces;
+    }
+
+
+    void evolve(const scalar dt)
+    {
+        vector<Vector> f = forces(positions_);
+
+	Vector center = Vector(0,0,0);
+	scalar totMass = 0.0;
+	for(label i=0; i<nBodies_; i++)
+	{
+	    totMass += mass_[i];
+	    center = center + mass_[i]*positions_[i];
 	}
-    } 
+	center = center/totMass;
 
-    return forces;
-  }
+        // update velocities
+        // m a = F
+        for(label i=0; i<nBodies_; i++)
+        {
+            velocities_[i] = velocities_[i] + dt*f[i]/mass_[i];
+        }
 
+        // update positions
+	Vector newCenter = Vector(0,0,0);
+        for(label i=0; i<nBodies_; i++)
+        {
+            positions_[i] = positions_[i] + dt*velocities_[i];
+	    newCenter = newCenter + mass_[i]*positions_[i];
+        }
+	newCenter = newCenter/totMass;
 
-  void evolve(const scalar dt)
-  {
-    vector<Vector> f = forces(positions());
+	Vector transpose = center - newCenter;
+	for(label i=0; i<nBodies_; i++)
+	{
+	    positions_[i] = positions_[i] + transpose;
+	}
 
-    // update velocities
-    // m a = F
-    for(label i=0; i<nBodies_; i++)
-    {
-	velocities_[i] = velocities_[i] + dt*f[i]/mass_[i];
     }
-
-    // update positions
-    for(label i=0; i<nBodies_; i++)
+  
+    void initMasses
+    (
+        const double minMass,
+        const double maxMass
+    )
     {
-	positions_[i] = positions_[i] + dt*velocities_[i];
+        for(label i=0; i<mass_.size(); i++)
+        {
+	    scalar mass = (maxMass-minMass)*((scalar)rand()/(scalar)RAND_MAX) + minMass;
+            mass_[i] = mass;
+        }
     }
-        
-  }
   
-  void initMasses
-  (
-    const double minMass,
-    const double maxMass
-  )
-  {
-    for(label i=0; i<mass_.size(); i++)
-      {
-	scalar mass = (maxMass-minMass)*((scalar)rand()/(scalar)RAND_MAX) + minMass;
-	mass_[i] = mass;
-      }
-  }
-  
-  void randomizeVectors
-  (
-    vector<Vector>& v, 
-    const double& mag
-  )
-  {
-    for(label i=0; i<v.size(); i++)
-      {
-	scalar x = ((scalar)rand()/(scalar)RAND_MAX) - 0.5;
-	scalar y = ((scalar)rand()/(scalar)RAND_MAX) - 0.5;
-	scalar z = ((scalar)rand()/(scalar)RAND_MAX) - 0.5;
-	z = 0.0;
-	Vector p(x, y, z);
-	scalar pMag = p.mag() + SMALL;
-	v[i] = p*mag/pMag;
-      }
-  }
+    void randomizeVectors
+    (
+        vector<Vector>& v, 
+        const double& mag
+    )
+    {
+        for(label i=0; i<v.size(); i++)
+        {
+            scalar x = ((scalar)rand()/(scalar)RAND_MAX) - 0.5;
+            scalar y = ((scalar)rand()/(scalar)RAND_MAX) - 0.5;
+            scalar z = ((scalar)rand()/(scalar)RAND_MAX) - 0.5;
+            z = 0.0;
+            Vector p(x, y, z);
+            scalar pMag = p.mag() + SMALL;
+            v[i] = p*mag/pMag;
+        }
+    }
 
 };
 
@@ -166,28 +186,55 @@ using namespace std;
 
 int main()
 {
-  scalar a = 1.0;
-  label nBodies = 3;
-  scalar minMass = 1.0;
-  scalar maxMass = 1000.0;
-  scalar maxRadius = 10000.0;
-  scalar maxVelocity = 10.0;
-  scalar endTime = 100.0;
-  scalar deltaT = 1.0e-1;
-  nBody system(nBodies);
-  system.initMasses(minMass, maxMass);
-  system.randomizeVectors(system.positions(), maxRadius);
-  system.randomizeVectors(system.velocities(), maxVelocity);
+    scalar a = 1.0;
+    label nBodies = 3;
+    scalar minMass = 1.0e+7;
+    scalar maxMass = 2.0e+7;
+    scalar maxRadius = 100.0;
+    scalar maxVelocity = 0.01;
+    label nSteps = 100000000;
+    scalar deltaT = 1.0e-2;
+    label nOut = nSteps / 100;
 
-  scalar time = 0.0;
-  while (time <= endTime)
+    nBody system(nBodies);
+    system.initMasses(minMass, maxMass);
+    system.randomizeVectors(system.positions(), maxRadius);
+    system.randomizeVectors(system.velocities(), maxVelocity);
+
+    for(label i=0; i<nBodies; i++)
     {
-      time += deltaT;
-      system.evolve(deltaT);
-      cout << "t = " << time << ": "
+        cout << "m[" << i << "] = " << system.mass()[i] << ", X = " << system.positions()[i] << ", U = " << system.velocities()[i] << endl;
+    }
+
+    ofstream file("pos.dat");
+    file.precision(10);
+    label nCount = 0;
+    for(label ii=0; ii<nSteps; ii++)
+    {
+        nCount++;
+        system.evolve(deltaT);
+	/*
+        cout << "t = " << time << ": "
 	   << system.positions()[0] << " : "
 	   << system.velocities()[0]
 	   << endl;
+	*/
+	if (nCount >= nOut)
+	{
+	    nCount = 0;
+	    file << system.positions()[0] << " "
+		 << system.positions()[1]
+		 << endl;
+	}
     }
-  return 0;
+    file.close();
+
+    cout << "==================" << endl;
+    for(label i=0; i<nBodies; i++)
+      {
+	cout << "m[" << i << "] = " << system.mass()[i] << ", X = " << system.positions()[i] << ", U = " << system.velocities()[i] << endl;
+      }
+
+    
+    return 0;
 }
